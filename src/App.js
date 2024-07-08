54d961e6b4f9f59c57bbba4179627d8c04c55315
jefersonsilva01 <jeferson.s.silva1@gmail.com>
2024-07-08 14:47:38 -0300

    Done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,140 @@
-import logo from './logo.svg';
+import { Component } from 'react';
 import './App.css';
+import dataContacts from "./contacts.json";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      contacts: dataContacts.filter((curr, index) => index < 5 && curr)
+    }
+  }
+
+  addRancomContact(newContacts) {
+    const index = Math.floor(Math.random() * (dataContacts.length - 5) + 5);
+    const randomContact = dataContacts[index];
+    newContacts.push(randomContact);
+    this.setState({
+      contacts: newContacts
+    })
+  }
+
+  sortByPopularity(newContacts) {
+    const sortContacts = newContacts.sort((a, b) => b.popularity - a.popularity);
+
+    this.setState({
+      contacts: sortContacts
+    })
+  }
+
+  sortByName(newContacts) {
+    const sortContacts = newContacts.sort((a, b) => {
+      return a.name.toLowerCase().localeCompare(b.name.toLowerCase())
+    });
+
+    this.setState({
+      contacts: sortContacts
+    })
+  }
+
+  deleteContact(index, newContacts) {
+    newContacts.splice(index, 1);
+    this.setState({
+      contacts: newContacts
+    })
+  }
+
+  render() {
+    const newContacts = [...this.state.contacts];
+
+    return (
+      <div className="App" style={{ margin: "16px auto" }}>
+        <h1>Iron Contact</h1>
+        <button
+          style={
+            {
+              padding: "8px"
+            }
+          }
+          onClick={
+            () => {
+              this.addRancomContact(newContacts)
+            }
+          }>
+          Add random contact
+        </button>
+        <button
+          style={
+            {
+              padding: "8px"
+            }
+          }
+          onClick={
+            () => {
+              this.sortByPopularity(newContacts)
+            }
+          }>
+          Sort by popularity
+        </button>
+        <button
+          style={
+            {
+              padding: "8px"
+            }
+          }
+          onClick={
+            () => {
+              this.sortByName(newContacts)
+            }
+          }>
+          Sort by name
+        </button>
+        <table style={{ margin: "16px auto" }}>
+          <tbody>
+            <tr>
+              <th>Picture</th>
+              <th>Name</th>
+              <th>Popularity</th>
+              <th>Won Oscar</th>
+              <th>Won Emmy</th>
+            </tr>
+            {
+              this.state.contacts.map((item, index) => {
+                return (
+                  <tr key={index}>
+                    <td>
+                      <img
+                        src={item.pictureUrl}
+                        style={
+                          {
+                            width: "70px",
+                            height: "100px",
+                            margin: "0 8px 0 0"
+                          }
+                        }
+                        alt="cover"
+                      />
+                    </td>
+                    <td>{item.name}</td>
+                    <td>{item.popularity.toFixed(2)}</td>
+                    <td>{item.wonOscar && <span>🏆</span>}</td>
+                    <td>{item.wonEmmy && <span>🏆</span>}</td>
+                    <td>
+                      <button style={
+                        {
+                          padding: "8px"
+                        }
+                      } onClick={() => { this.deleteContact(index, newContacts) }}>Delete</button>
+                    </td>
+                  </tr>
+                )
+              })
+            }
+          </tbody>
+        </table>
+      </div>
+    );
+  }
 }
 
 export default App;
